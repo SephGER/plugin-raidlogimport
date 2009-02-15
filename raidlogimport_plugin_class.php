cd910617ce08a9d18963caa12f6dcea823b8245e
hoofy_leon <hoofy_leon@eqdkp-plus.eu>
2009-02-15 11:58:24 +0000

    forgot rev.

--- raidlogimport_plugin_class.php
+++ raidlogimport_plugin_class.php
@@ -19,7 +19,7 @@ class raidlogimport_Plugin_Class extends EQdkp_Plugin
 {
 	var $vstatus = 'Stable';
 	var $version = '0.4.5';
-	var $build = 3816;
+	var $build = 3821;
 
     function raidlogimport_plugin_class($pm)
     {
