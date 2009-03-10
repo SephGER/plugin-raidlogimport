5f0c2202a60ff6ec45b0bb8e7798f9976a8975fa
hoofy_leon <hoofy_leon@eqdkp-plus.eu>
2009-03-10 20:19:14 +0000

    another little bug

--- admin/dkp.php
+++ admin/dkp.php
@@ -99,7 +99,7 @@ class raidlogimport extends EQdkp_Admin
 		{
 			if($_POST['checkraid'] == $user->lang['rli_calc_note_value'])
 			{
-				$rai['value'] = $rli->get_raidvalue($rai['begin'], $rai['end'], $rai['bosskills'], $rai['timebonus']);
+				$rai['value'] = $rli->get_raidvalue($rai['begin'], $rai['end'], $rai['bosskills'], $rai['timebonus'], $rai['event']);
 				if($rai['bosskills'] AND $rli->config['raidcount'] != 2)
 				{
 					$rai['note'] = $rli->get_note($rai['bosskills'], true);
--- raidlogimport_plugin_class.php
+++ raidlogimport_plugin_class.php
@@ -21,7 +21,7 @@ class raidlogimport_Plugin_Class extends EQdkp_Plugin
 	var $version = '0.5.1.7';
 	var $fwversion = '1.0.3';
 	var $jqversion = '2.0.1';
-	var $build = 4172;
+	var $build = 4173;
 
     function raidlogimport_plugin_class($pm)
     {
