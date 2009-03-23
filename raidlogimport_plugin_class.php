2c63ffc47e21f3c334128aec280de19b9b04757c
hoofy_leon <hoofy_leon@eqdkp-plus.eu>
2009-03-23 14:49:12 +0000

    typo

--- raidlogimport_plugin_class.php
+++ raidlogimport_plugin_class.php
@@ -21,7 +21,7 @@ class raidlogimport_Plugin_Class extends EQdkp_Plugin
 	var $version = '0.5.3.1';
 	var $fwversion = '1.0.3';
 	var $jqversion = '2.0.1';
-	var $build = 4343;
+	var $build = 4345;
 
     function raidlogimport_plugin_class($pm)
     {
