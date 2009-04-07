6d5a7639f9e53bbec091c113760b560cb9ac058c
hoofy_leon <hoofy_leon@eqdkp-plus.eu>
2009-04-07 10:11:47 +0000

    fixed member_display without gd-lib

--- raidlogimport_plugin_class.php
+++ raidlogimport_plugin_class.php
@@ -18,7 +18,7 @@ if ( !defined('EQDKP_INC') )
 class raidlogimport_Plugin_Class extends EQdkp_Plugin
 {
 	var $vstatus = 'Stable';
-	var $version = '0.5.3.1';
+	var $version = '0.5.3.2';
 	var $fwversion = '1.0.3';
 	var $jqversion = '2.0.1';
 	var $build = 4352;
