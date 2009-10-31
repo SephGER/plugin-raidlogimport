7a44952bc92b28538e73a0365fd7f513f2b88e9a
hoofy_leon <hoofy_leon@eqdkp-plus.eu>
2009-10-31 10:51:18 +0000

    fix missing ','

--- raidlogimport_plugin_class.php
+++ raidlogimport_plugin_class.php
@@ -37,7 +37,7 @@ class raidlogimport_Plugin_Class extends EQdkp_Plugin
     {
         global $eqdkp_root_path, $user, $SID, $conf_plus, $eqdkp;
 
-        $this->build = 6303;
+        $this->build = 6315;
 
         $this->eqdkp_plugin($pm);
         $this->pm->get_language_pack('raidlogimport');
