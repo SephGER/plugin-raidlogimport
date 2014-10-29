f85ebbbbb3b7da309995b10b0fb0845a2bc755ee
godmod <godmod@eqdkp-plus.eu>
2014-10-29 16:37:04 +0000

    Created Tag 0.6.3.5 for RLI

---
+++ language/english/wow_lang.php
@@ -0,0 +1,38 @@
+<?php
+ /*
+ * Project:     EQdkp-Plus Raidlogimport
+ * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:       2008
+ * Date:        $Date: 2013-01-23 20:27:42 +0100 (Mi, 23 Jan 2013) $
+ * -----------------------------------------------------------------------
+ * @author      $Author: hoofy_leon $
+ * @copyright   2008-2009 hoofy_leon
+ * @link        http://eqdkp-plus.com
+ * @package     raidlogimport
+ * @version     $Rev: 12892 $
+ *
+ * $Id: wow_lang.php 12892 2013-01-23 19:27:42Z hoofy_leon $
+ */
+$lang['hero'] = 'Suffix for Heroic Events';
+$lang['non_hero'] = 'Suffix for Non-Heroic Events';
+$lang['title_difficulty'] = 'Difficulty settings';
+$lang['difficulty'] = 'Difficulty';
+$lang['diff_0'] = 'other';
+$lang['diff_1'] = 'normal';
+$lang['diff_2'] = 'heroic';
+$lang['diff_3'] = '10 players normal';
+$lang['diff_4'] = '25 players normal';
+$lang['diff_5'] = '10 players heroic';
+$lang['diff_6'] = '25 players heroic';
+$lang['diff_7'] = 'LFR';
+$lang['diff_8'] = 'Challenge Mode';
+$lang['diff_9'] = '40 players';
+$lang['diff_11'] = 'Heroic';
+$lang['diff_12'] = 'Normal';
+$lang['diff_14'] = 'Normal (10-30 players)';
+$lang['diff_15'] = 'Heroic (10-30 players)';
+$lang['diff_16'] = 'Mythic (20 players)';
+$lang['dep_match'] = 'Shall the suffix be applied to boss-note, too?';
+?>
---
+++ language/german/wow_lang.php
@@ -0,0 +1,36 @@
+<?php
+ /*
+ * Project:     EQdkp-Plus Raidlogimport
+ * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:       2008
+ * Date:        $Date: 2012-11-11 16:50:58 +0100 (So, 11 Nov 2012) $
+ * -----------------------------------------------------------------------
+ * @author      $Author: wallenium $
+ * @copyright   2008-2009 hoofy_leon
+ * @link        http://eqdkp-plus.com
+ * @package     raidlogimport
+ * @version     $Rev: 12431 $
+ *
+ * $Id: wow_lang.php 12431 2012-11-11 15:50:58Z wallenium $
+ */
+$lang['difficulty'] = 'Schwierigkeit';
+$lang['title_difficulty'] = 'Schwierigkeits-Einstellungen';
+$lang['diff_0'] = 'beliebig';
+$lang['diff_1'] = 'normal';
+$lang['diff_2'] = 'heroisch';
+$lang['diff_3'] = '10 Spieler normal';
+$lang['diff_4'] = '25 Spieler normal';
+$lang['diff_5'] = '10 Spieler heroisch';
+$lang['diff_6'] = '25 Spieler heroisch';
+$lang['diff_7'] = 'LFR';
+$lang['diff_8'] = 'Herausforderungsmodus';
+$lang['diff_9'] = '40 Spieler';
+$lang['diff_11'] = 'Heroisch';
+$lang['diff_12'] = 'Normal';
+$lang['diff_14'] = 'Normal (10-30 Spieler)';
+$lang['diff_15'] = 'Heroisch (10-30 Spieler)';
+$lang['diff_16'] = 'Mythisch (20 Spieler)';
+$lang['dep_match'] = 'Soll den Boss-Notizen der Suffix angehängt werden?';
+?>
