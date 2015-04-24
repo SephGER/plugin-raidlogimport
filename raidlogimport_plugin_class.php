2223eca589d8eeea931d37ff76c040ad6d2c2525
God Mod <godmod23@web.de>
2015-04-24 09:27:55 +0200

    Bumped Version

--- raidlogimport_plugin_class.php
+++ raidlogimport_plugin_class.php
@@ -30,7 +30,7 @@ class raidlogimport extends plugin_generic {
 	}
 
 	public $vstatus = 'Beta';
-	public $version = '0.7.0.4';
+	public $version = '0.7.1.0';
 	
 	protected static $apiLevel = 20;
 	
